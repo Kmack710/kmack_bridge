cdadfe827ed8535e72a695b9a617e53c1f4c753b
Kmack710 <69439573+Kmack710@users.noreply.github.com>
2024-07-31 17:38:02 -0400

    ox_core serverside support done

--- config.lua
+++ config.lua
@@ -1,11 +1,11 @@
 return {
     config = {
         --- Phone script (yseries, lb any others please request or make a PR)
-        PhoneScript = "yseries",
+        PhoneScript = "npwd",
         --- Inventory script (ox, qb, qs and core any others please request or make a PR)
         InventoryScript = "ox",
         --- Framework (qb, qbx or esx - sandbox framework support to come soon.)
-        Framework = "qbx",
+        Framework = "ox",
         --- Notifications (qb, esx, ox or custom - Check the notifications folder for more info)
         Notifications = "ox",
     }
---
+++ frameworks/server/ox.lua
@@ -0,0 +1,67 @@
+local Ox = require '@ox_core.lib.init'
+print('^4 [kmack_bridge] ^1 Loaded OX Framework')
+local Framework = {}
+
+function Framework.PlayerDataS(source)
+    local data = Ox.GetPlayer(source)
+    if data ~= nil then
+        local groups = data.getGroups()
+        local groupName, grade = data.getGroup(groups)
+        local pJob = {
+            name = groupName,
+            label = GlobalState['group.'..groupName].label,
+            Grade = {
+                name = string.lower(GlobalState['group.'..groupName].grades[grade]),
+                label = GlobalState['group.'..groupName].grades[grade],
+                level = grade
+            }
+        }
+        local accounts = data.getAccounts()
+        local accountid
+        for k,v in pairs(accounts) do
+            if v.isDefault and v.type == 'personal' then
+                accountid = v.id
+                break
+            end
+        end
+        local Pdata = {
+            Pid = data.stateid,
+            Name = data.username,
+            Identifier = data.identifier,
+            Bank = data.getAccount(accountid).balance,
+            Cash = function() return exports.ox_inventory:Search(source, 'count', 'money') end,
+            Source = data.source,
+            Job = pJob,
+            AddBankMoney = function(amount) Ox.DepositMoney(source, accountid, amount) end,
+            RemoveBankMoney = function(amount) Ox.WithdrawMoney(source, accountid, amount) end,
+            AddCash = function(amount) exports.ox_inventory:AddItem(source, 'money', amount) end,
+            RemoveCash = function(amount) exports.ox_inventory:RemoveItem(source, 'money', amount) end,
+            SetJob = function(job, grade) data.setGroup(job, grade) end,
+            Gang = false, --- no gang support in ox yet that i know of. 
+            SetGang = false,
+        }
+        return Pdata
+    else
+        return false
+    end
+end
+
+function Framework.GetPlayerFromPidS(pid)
+    local Player = Ox.GetCharIdFromStateId(pid)
+    if Player then
+        return Player
+    else
+        return false
+    end
+end
+
+function Framework.CreateUseableItem(item, cb)
+    ---- Not used as we do so directly in ox_inventory or other inventory systems.
+end
+
+function Framework.OfflinePlayerPayment(pid, amount)
+
+
+end
+
+return Framework
